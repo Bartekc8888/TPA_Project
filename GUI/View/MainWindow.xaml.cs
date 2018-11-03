3a1fdd8a0efdfd8946b24da21972b82e68be9c1c
Bartłomiej Ciesielski <bartekc8@op.pl>
2018-11-03 14:09:35 +0100

    Init GUI project

---
+++ GUI/View/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+
+namespace GUI
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PathButton_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog
+            {
+                // Set filter for file extension and default file extension 
+                DefaultExt = ".exe",
+                Filter = "Executable Files (*.exe)|*.exe|DLL Files (*.dll)|*.dll"
+            };
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dialog.ShowDialog();
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                string filename = dialog.FileName;
+                this.PathText.Text = filename;
+            }
+        }
+
+        private void AnalizeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
