d27c04adad45f607df2fe9e5b1be7a0a9b9c90d0
michalszwarocki <44211267+michalszwarocki@users.noreply.github.com>
2018-12-05 20:35:52 +0100

    Add logging test

---
+++ ModelTest/FileLoggingTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ModelTest
+{
+    [TestClass]
+    public class FileLoggingTest
+    {
+        [TestMethod]
+        public void CheckIfLoggingIsCorrect()
+        {
+            FileInfo fileTest = new FileInfo("loggingTest.txt");
+            if (fileTest.Exists)
+            {
+                fileTest.Delete();
+            }
+
+            ILogging logger = new FileLogging("loggingTest.txt", "Logging test");
+            logger.Debug("deb");
+            logger.Error("err");
+            logger.Fatal("fat");
+            logger.Info("inf");
+            logger.Warn("war");
+
+            fileTest.Refresh();
+            Assert.IsTrue(fileTest.Exists);
+            Assert.AreEqual("loggingTest.txt", fileTest.Name);
+            Assert.IsTrue(fileTest.Length > 30);
+        }
+    }
+}
